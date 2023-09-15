e9a08a8b3d54046d7a8f9e861903c3176d84469e
Egor Diomin <diomin.egor@gmail.com>
2023-09-15 16:04:50 +0300

    completed

---
+++ Task41/Program.cs
@@ -0,0 +1,53 @@
+﻿﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+void FillArray (int[] arr)
+{
+    Console.WriteLine($"Введите числа для массива:");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Convert.ToInt32(Console.ReadLine());  
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int PositiveNumbersCount(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] < 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+if (size < 1)
+{
+    Console.WriteLine("Размер не может быть <=0.");
+}
+int[] array = new int[size];
+
+FillArray(array);
+Console.WriteLine();
+PrintArray(array);
+int result = PositiveNumbersCount(array);
+Console.WriteLine();
+Console.WriteLine($"В массиве {result} чисел(-ла) больше 0.");
---
+++ Task43/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// k1*x + b1 = k2*x + b2
+
+double[] IntersectionPoint(double k1, double b1, double k2, double b2)
+{
+    double[] coord = new double[2];
+    var x = -(b1 - b2) / (k1 - k2);
+    var y = k1 * x + b1;
+    coord[0] = x;
+    coord[1] = y;
+    return coord;
+}
+
+Console.Write("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+
+double[] position = IntersectionPoint(k1, b1, k2, b2);
+Console.WriteLine($"Точка пересечения: {position[0]}, {position[1]}");
